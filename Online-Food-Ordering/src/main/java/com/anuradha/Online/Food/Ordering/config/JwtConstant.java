5c4b6e1849f3b69137fe0e925bebe190208254bf
De Silva Anuradha <desilvaanuradha@Des-MacBook-Pro.local>
2024-07-30 20:45:46 +0530

    Implement App configeration

---
+++ Online-Food-Ordering/src/main/java/com/anuradha/Online/Food/Ordering/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.anuradha.Online.Food.Ordering.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "YOUR_JWT_SECRET";
+    public static final String JWT_HEADER = "Authorization";
+}
