059518744d0515931573082f534d25854cf1c89e
De Silva Anuradha <anuradhashashimal2@gmail.com>
2024-12-15 14:17:24 +0530

    state manage of cart

--- online-food-order-web/src/component/State/store.js
+++ online-food-order-web/src/component/State/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { authReducer } from "./Reducer";
 import { thunk } from "redux-thunk";
 import { restaurantReducer } from "../Restaurant/Reducer";
+import { menuItemReducer } from "../Menu/Reducer";
 
 const rooteReducer = combineReducers({
     auth: authReducer,
-    reastaurat: restaurantReducer
+    reastaurat: restaurantReducer,
+    menu: menuItemReducer
 });
 export const store = legacy_createStore(rooteReducer, applyMiddleware(thunk))
