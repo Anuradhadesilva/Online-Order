7f639c0f91e03c8dd8d815445a04b3e0f9bd33dd
De Silva Anuradha <anuradhashashimal2@gmail.com>
2025-03-02 12:56:34 +0530

    state management of restaurant

--- online-food-order-web/src/component/State/Restaurant Order/Reducer.js
+++ online-food-order-web/src/component/State/Restaurant Order/Reducer.js
@@ -1,7 +1,30 @@
-export const GET_RESTAURANTS_ORDER_REQUEST = "GET_RESTAURANTS_ORDER_REQUEST";
-export const GET_RESTAURANTS_ORDER_SUCCESS = "GET_RESTAURANTS_ORDER_SUCCESS";
-export const GET_RESTAURANTS_ORDER_FAILURE = "GET_RESTAURANTS_ORDER_FAILURE";
+import { GET_RESTAURANTS_ORDER_FAILURE, GET_RESTAURANTS_ORDER_REQUEST, GET_RESTAURANTS_ORDER_SUCCESS, UPDATE_ORDER_STATUS_FAILURE, UPDATE_ORDER_STATUS_REQUEST, UPDATE_ORDER_STATUS_SUCCESS } from "./ActionType";
 
-export const UPDATE_ORDER_STATUS_REQUEST = "UPDATE_ORDER_STATUS_REQUEST";
-export const UPDATE_ORDER_STATUS_SUCCESS = "UPDATE_ORDER_STATUS_SUCCESS";
-export const UPDATE_ORDER_STATUS_FAILURE = "UPDATE_ORDER_STATUS_FAILURE";
+
+const initialState = {
+    loading: false,
+    orders: [],
+    error: null,
+};
+
+export const restaurantsOrderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_RESTAURANTS_ORDER_REQUEST:
+        case UPDATE_ORDER_STATUS_REQUEST:
+            return { ...state, error: null, loading: true };
+        case GET_RESTAURANTS_ORDER_SUCCESS:
+            return { ...state, loading: false, orders: action.payload };
+        case UPDATE_ORDER_STATUS_SUCCESS:
+            const updateOrders = state.orders.map((order) =>
+                order.id === action.payload.id ? action.payload : order
+            );
+            return { ...state, loading: false, orders: updateOrders };
+        case GET_RESTAURANTS_ORDER_FAILURE:
+        case UPDATE_ORDER_STATUS_FAILURE:
+            return { ...state, loading: false, orders: updateOrders };
+
+        default:
+            return state;
+    }
+};
+
